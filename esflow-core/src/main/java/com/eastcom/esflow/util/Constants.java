dcd01bd639b8c322623f8b6d26108b0ffe60e8d0
lukw510903926 <13507615840@163.com>
2017-01-01 19:30:00 +0800

    配置文件优化

--- esflow-core/src/main/java/com/eastcom/esflow/util/Constants.java
+++ esflow-core/src/main/java/com/eastcom/esflow/util/Constants.java
@@ -11,29 +11,12 @@ public class Constants {
 	 * 签收标识
 	 */
 	public final static String SIGN = "SIGN";
-	/**
-	 * 延期标识
-	 */
-	public final static String DELAY_TIME = "DELAYTIME";
-	
-	/**
-	 * 是否延期
-	 */
-	public final static String IS_ASSIGNSUBBIZ = "IS_ASSIGNSUBBIZ";
-	/**
-	 * 延期不通过
-	 */
-	public final static String DELAY_NOT_PASS = "DELAYNOTPASS";
-	
+
 	/**
 	 * 处理标识
 	 */
 	public final static String HANDLE = "HANDLE";
-	/**
-	 * 转派
-	 */
-	public final static String TURNSEND = "转派";
-	
+
 	
 	//============== 流程参数相关 =============
 	/**
@@ -45,16 +28,6 @@ public class Constants {
 	 */
 	public final static String SYS_BIZ_ID = "SYS_BIZ_ID";
 	
-	//=====================变更流程操作类型===========================
-	
-	public final static String  OPERATE_ADD= "新增";
-	
-	public final static String OPERATE_UPDATE = "更新";
-	
-	public final static String OPERATE_DELETE = "删除";
-	
-	public final static String OPERATE_TEMP = "tempType";
-	
 	//=====================工单状态===========================
 	
 	//草稿
@@ -62,12 +35,9 @@ public class Constants {
 	//新建
 	public final static String BIZ_NEW = "新建";
 	
-	public final static String BIZ_HANDLE = "处理中";
 	//结束
 	public final static String BIZ_END = "已完成";
 	
-	//申请人处理
-	public final static String BIZ_CREATEUSER = "申请人处理";
 	//删除
 	public final static String BIZ_DELETE = "已删除";
 	//拦截
@@ -79,10 +49,4 @@ public class Constants {
 	public final static int MEET_YES = 1;
 	//驳回
 	public final static int MEET_NO = 0;
-	
-//	==================工单来源===============================
-	// 新平台
-	public final static String NEW_PLATFORM= "运维管理";
-	//旧平台
-	public final static String OLD_PLATFORM = "12583";
 }
